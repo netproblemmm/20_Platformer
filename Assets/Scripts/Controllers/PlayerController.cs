9be41549ca16e0747778752bef2d3c834c091453
netproblemmm <ubhawk1@mail.ru>
2021-09-26 19:13:15 +0300

    Homework#3 1. adding Cannon and bullets with trails 2. change physics on Box2D. 3. fix some bugs with animation tracks

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -7,7 +7,7 @@ namespace PlatformerMVC
         private float _xAxisInput;
         private bool _isJump;
 
-        private float _walkSpeed = 8f;
+        private float _walkSpeed = 100f;
         private float _animationSpeed = 10f;
         private float _movingThreshold = 0.1f;
 
@@ -19,36 +19,34 @@ namespace PlatformerMVC
 
         private float _jumpForce = 9f;
         private float _jumpThreshold = 1f;
-        private float _g = -9.8f;
-        private float _groundLevel = -1.8f;
         private float _yVelocity = 0f;
         private float _xVelocity = 0f;
 
         private LevelObjectView _view;
         private SpriteAnimatorController _spriteAnimator;
-        // pooler
+        private readonly ContactPoller _contactPoller; 
 
         public PlayerController(LevelObjectView player, SpriteAnimatorController animator)
         {
             _view = player;
             _spriteAnimator = animator;
             _spriteAnimator.StartAnimation(_view._spriteRenderer, AnimState.Idle, true, _animationSpeed);
+            _contactPoller = new ContactPoller(_view._collider);
         }
 
         private void MoveTowards()
         {
-            _view.transform.position += _vectorRight * (Time.deltaTime * _walkSpeed * (_xAxisInput < 0 ? -1: 1));
+            _xVelocity = _walkSpeed * Time.fixedDeltaTime * (_xAxisInput < 0 ? -1 : 1);
+            _view._rigidbody.velocity = _view._rigidbody.velocity.Change(x: _xVelocity);
             _view.transform.localScale = (_xAxisInput < 0 ? _leftScale : _rightScale);
-        }
-
-        public bool IsGrounded()
-        {
-            return _view.transform.position.y <= _groundLevel && _yVelocity <= 0;
         }
 
         public void Update()
         {
             _spriteAnimator.Update();
+            _contactPoller.Update();
+
+            _yVelocity = _view._rigidbody.velocity.y;
             _xAxisInput = Input.GetAxis("Horizontal");
             _isJump = Input.GetAxis("Vertical") > 0;
             isMoving = Mathf.Abs(_xAxisInput) > _movingThreshold;
@@ -58,28 +56,21 @@ namespace PlatformerMVC
                 MoveTowards();
             }
 
-            if (IsGrounded())
+            if (_contactPoller.IsGrounded)
             {
-                _spriteAnimator.StartAnimation(_view._spriteRenderer , isMoving ? AnimState.Run : AnimState.Idle, true, _animationSpeed);
+                _spriteAnimator.StartAnimation(_view._spriteRenderer, isMoving ? AnimState.Run : AnimState.Idle, true, _animationSpeed);
 
-                if(_isJump && _yVelocity == 0)
+                if(_isJump && Mathf.Abs(_yVelocity) <= _jumpThreshold)
                 {
-                    _yVelocity = _jumpForce;
-                }
-                else if(_yVelocity < 0)
-                {
-                    _yVelocity = 0;
-                    _view._transform.position = _view._transform.position.Change(y: _groundLevel);
+                    _view._rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
                 }
             }
             else
             {
                 if(Mathf.Abs(_yVelocity) > _jumpThreshold)
                 {
-                    _spriteAnimator.StartAnimation(_view._spriteRenderer, AnimState.Jump, true, _animationSpeed);
+                    _spriteAnimator.StartAnimation(_view._spriteRenderer, AnimState.Jump, false, _animationSpeed);
                 }
-                _yVelocity += _g * Time.deltaTime;
-                _view._transform.position += Vector3.up * (_yVelocity * Time.deltaTime);
             }
         }
     }
